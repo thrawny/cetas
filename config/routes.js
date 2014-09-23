bd83ddcfe63bd86429232584e8d366c49120fae2
Jonas Lergell <jonaslergell@gmail.com>
2014-09-23 15:42:51 +0200

    removed routing to profile. we will remove it

--- config/routes.js
+++ config/routes.js
@@ -27,7 +27,7 @@ module.exports = function (app, passport) {
     .get(user.signup);
 
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/profile', // redirect to the secure profile section
+    successRedirect : '/', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
