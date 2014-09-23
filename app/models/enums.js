dda20f960287963b3dd569e8218003e9a8a0e3a6
Chefenmike <Mikael666jo@hotmail.com>
2014-09-23 10:57:59 +0200

    Added enums

---
+++ app/models/enums.js
@@ -0,0 +1,29 @@
+/**
+ * New node file
+ */
+"use strict";
+
+var Enum = require('enum');
+
+var yesOrNo = new Enum({
+	'yes' : 'Yes',
+	'no' : 'No'
+});
+
+var pain = new Enum({
+	'1' : 'Ingen smärta',
+	'2' : 'Neutral',
+	'3' : 'Värsta tänkbara'
+});
+
+var myEnum = new Enum({
+	'A' : '1',
+	'B' : '2',
+	'C' : 4
+});
+
+var myEnum = new Enum({
+	'A' : '1',
+	'B' : '2',
+	'C' : 4
+});
