264ac2ae5daa286f4d09a1959a78c319cb654f56
Chefenmike <Mikael666jo@hotmail.com>
2014-10-01 19:53:39 +0200

    Två different dummy-messages, one for patient and one for doctor.

--- config/routes.js
+++ config/routes.js
@@ -3,82 +3,75 @@ var user = require('../app/controllers/user');
 var mypatients = require('../app/controllers/mypatients');
 var auth = require('./auth');
 
-module.exports = function (app, passport) {
+module.exports = function(app, passport) {
 
+	// Authorize user to a route. Check auth.js for logic.
+	app.use(function(req, res, next) {
+		if (auth.check(req)) {
+			next();
+		} else {
+			res.status(403).end('Access denied. NOT API.');
+		}
+	});
 
-  // Authorize user to a route. Check auth.js for logic.
-  app.use(function(req, res, next) {
-    if (auth.check(req)) {
-      next();  
-    }
-    else {
-      res.status(403).end('Access denied. NOT API.');
-    }
-  });
+	app.use('/api', function(req, res, next) {
+		if (auth.checkAPI(req)) {
+			next();
+		} else {
+			res.status(403).end('Access denied.');
+		}
+	});
 
-  app.use('/api', function (req, res, next) {
-    if(auth.checkAPI(req)) {
-      next();
-    }
-    else {
-      res.status(403).end('Access denied.');
-    }
-  });
+	app.route('/').get(home.list).post(home.create);
 
-	app.route('/')
-    .get(home.list)
-    .post(home.create);
+	app.route('/form').get(home.form).post(home.create);
 
-	app.route('/form')
-    .get(home.form)
-    .post(home.create);
+	app.route('/article/:articleId').get(home.view);
 
-  app.route('/article/:articleId')
-    .get(home.view);
+	app.route('/delete/:articleId').get(home.destroy);
 
-  app.route('/delete/:articleId')
-    .get(home.destroy);
+	app.route('/mypatients').get(mypatients.list);
 
-  app.route('/mypatients')
-    .get(mypatients.list);
+	// User routes
+	app.route('/login').get(user.login)
 
-  // User routes
-  app.route('/login')
-    .get(user.login)
+	app.route('/logout').get(user.logout);
 
-  app.route('/logout')
-    .get(user.logout);
+	app.route('/signup').get(user.signup);
 
-  app.route('/signup')
-    .get(user.signup);
+	app.route('/profile').get(user.view);
 
-  // Dummy api routes. Replace these with the real stuff. The '/api' middleware will handle authorization. Check line 18 in this file.
-  app.route('/api/patients/:id')
-    .get(function(req, res) {
-      var data = { patient: 'name'+req.params.id }
-      res.json(data);
-    });
+	// Dummy api routes. Replace these with the real stuff. The '/api'
+	// middleware will handle authorization. Check line 18 in this file.
+	app.route('/api/patients/:id').get(function(req, res) {
+		var data = {
+			patient : 'name' + req.params.id
+		}
+		res.json(data);
+	});
 
-  app.route('/api/patients')
-    .get(function(req, res) {
-      var data = {
-        patients: ['patient1', 'patient2', 'patient3']
-      };
-      res.json(data);
-    });
+	app.route('/api/patients').get(function(req, res) {
+		var data = {
+			patients : [ 'patient1', 'patient2', 'patient3' ]
+		};
+		res.json(data);
+	});
 
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-  }));
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if
+										// there is an error
+		failureFlash : true
+	// allow flash messages
+	}));
 
-  // process the login form
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-  }));
+	// process the login form
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there
+									// is an error
+		failureFlash : true
+	// allow flash messages
+	}));
 
 };
-
