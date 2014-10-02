affc6c4ee4715dfadad6bde23f855ed7db842368
Sundin <sundin666@gmail.com>
2014-10-02 15:18:46 +0200

    Added authorization for listing patients.

--- app/controllers/mypatients.js
+++ app/controllers/mypatients.js
@@ -1,4 +1,5 @@
-﻿var data = {
+﻿// TODO: fetch from database
+var data = {
     "patients": [
         {"id": "910411-0573", "sex": "Man", "records": [{"id":"Form1", "1": "5.23", "2": "8.81"}, {"id": "Form2", "1": "4.32"}]},
         {"id": "910411-0574", "sex": "Kvinna", "records": [{"id":"Form1", "1": "3.33"}]},
@@ -6,6 +7,14 @@
     ]
 };
 
-module.exports.list = function(req, res, next){
+// Lists all patients
+module.exports.list = function(req, res, next) {
+    res.render('mypatients', {"patients": data.patients});
+};
+
+// Shows a specific patient
+module.exports.showPatient = function(req, res, next) {
+	// TODO: implement
+	// Get the id supplied with the URL using: req.params.id
     res.render('mypatients', {"patients": data.patients});
 };
--- config/auth.js
+++ config/auth.js
@@ -6,18 +6,21 @@ var path = require('path');
 
 // Object with routes and their accepted roles. If a route is not here it is open to all roles.
 var access = {
-  '/form': [roles.admin, roles.doctor, roles.patient]
+  '/form': [roles.admin, roles.doctor, roles.patient],
+  '/profile': [roles.admin, roles.doctor, roles.patient]
 }
 
+// Who can access different API routes (includes subroutes). If not in the list, nobody can access it.
 var api_access = {
   'patients': [roles.doctor]
 }
+
 // Check if a user is permitted to access the page.
 module.exports.check = function(req) {
   if (access[req.url]) {
     return req.isAuthenticated() &&  access[req.url].indexOf(req.user.role) > -1;
   }
-  return true;
+  return true; // If the page was not found in the list, allow everyone to view it.
 }
 
 // Authorize api routes
@@ -25,7 +28,7 @@ module.exports.checkAPI = function(req) {
   if (api_access[getBaseUrl(req.url)]) {
     return req.isAuthenticated() && api_access[getBaseUrl(req.url)].indexOf(req.user.role) > -1;   
   }
-  return false;
+  return false; // Block access if the api route was not found in api_access
 }
 
 function getBaseUrl(path) {
