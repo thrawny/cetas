7849cea031250d56687d8492fcd27f8f54515e15
Jonas Lergell <jonaslergell@gmail.com>
2014-10-17 10:32:42 +0200

    added dummydata.js to add doctor to db

---
+++ dummydata.js
@@ -0,0 +1,47 @@
+var config = require('./config/config');
+var mongoose = require('mongoose');
+var glob = require('glob');
+
+mongoose.connect(config.db, config.options);
+var db = mongoose.connection;
+
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + config.db);
+});
+
+var models = glob.sync(config.root + '/app/models/*.js');
+models.forEach(function (model) {
+  require(model);
+});
+
+var User = mongoose.model('User');
+
+var drUser = new User();
+
+User.findOne({'local.email': 'dr@cetas.se'}, function(err, user) {
+  if (err) {
+    console.log(err)
+    process.exit(1);
+  }
+  else if (user) {
+    console.log("User already exists");
+    process.exit();
+  }
+  else {
+    drUser.local.email = 'dr@cetas.se';
+    drUser.local.password = drUser.generateHash('password123');
+    drUser.role = 1;
+
+    drUser.save(function(err) {
+      if (err) {
+        console.log(err);  
+        process.exit(1);  
+      }
+      else {
+        console.log("Success");
+        process.exit();
+      }
+    });    
+  }
+});
+
