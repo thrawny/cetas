257bccf18193210390a32e940040a87078992985
Jonas Lergell <jonaslergell@gmail.com>
2014-11-25 12:47:15 +0100

    can change password

--- public/app/password/password.controller.js
+++ public/app/password/password.controller.js
@@ -12,13 +12,18 @@ angular.module('myApp')
         $scope.language = data;
       })
 
-    $scope.pass = {}
+    $scope.pass = { id: $scope.user._id };
 
     $scope.submit = function (form) {
       if (form.$valid) {
-        if ($scope.pass.password === $scope.pass.confirm) {
-          $http
-          console.log('valid');
+        if ($scope.pass.password === $scope.pass.confirmpassword) {
+          $http.post('/password', $scope.pass)
+            .success(function(data, status, headers, config) {
+              $state.go('index');
+            });
+          
+        } else {
+          $scope.message = "Lösenorden stämmer inte överens."
         }
       } else {
         console.log('invalid');
